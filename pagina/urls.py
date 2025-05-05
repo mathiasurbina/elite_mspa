ee7281e0f97f59c6f989bce51f128b56d23722fb
mathias urbina <matthiasurbina@gmail.com>
2025-05-05 01:10:54 -0400

    actualizacion

--- pagina/urls.py
+++ pagina/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('reserva/', reserva, name='reserva'),
     path('admin_panel/', admin_panel, name='admin_panel'),
     path('panel_reservas/', panel_reservas, name='panel_reservas'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
