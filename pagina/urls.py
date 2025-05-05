ba4dd59f14a2f83d83503aff9903936a1da2ab43
mathias urbina <matthiasurbina@gmail.com>
2025-05-05 01:16:35 -0400

    Actualización del proyecto

--- pagina/urls.py
+++ pagina/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import home, reserva, admin_panel, panel_reservas
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name='home'),
